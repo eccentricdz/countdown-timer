30af654a1eb4e0dcef159de3b9c8177d21c6201c
Rahul Agarwal <simplyrahul93@gmail.com>
2020-07-15 23:54:08 +0530

    Initial setup

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Theme from "./theme.json";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Oswald_500Medium,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [oswaldFontAvailable] = useFonts({ Oswald_500Medium, Oswald_400Regular });
+  if (oswaldFontAvailable) {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.text]}>Timer</Text>
+        <StatusBar style="inverted"></StatusBar>
+      </View>
+    );
+  } else {
+    return <Text>Loading</Text>;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Theme.backgroundColor,
+    alignItems: "center",
+    padding: 30,
+    paddingTop: 60,
+  },
+  text: {
+    textTransform: "uppercase",
+    fontFamily: "Oswald_500Medium",
+    color: "white",
+    fontSize: 24
+  },
+  regularText: {
+    fontFamily: "Oswald_400Regular",
   },
 });
